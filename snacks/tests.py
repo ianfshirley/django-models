300b07b3d775062926bce178971cacd61435466c
ianfshirley <ianfshirley88@gmail.com>
2023-01-30 19:58:34 -0800

    added models, urls, views to snacks app

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class Test(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        print(f'the url is: {url}')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_snack_list_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_home_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Snack
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+class SnackDetailsView(DetailView):
+    template_name = 'snack_details.html'
+    model = Snack
